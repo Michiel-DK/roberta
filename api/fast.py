70f4c0bceb42bee7fae3236425f69f731496458b
Michiel De Koninck <michieldekoninck2@gmail.com>
2022-03-11 11:39:38 +0000

    update get txt

--- api/fast.py
+++ api/fast.py
@@ -13,6 +13,8 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+
+
 @app.get("/")
 def index():
     return {"greeting": "Hello world"}
--- roberta/base.py
+++ roberta/base.py
@@ -1,26 +1,43 @@
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 import joblib
 import os
+import pdfplumber
+
 
 ROOT_DIR = os.path.dirname(__file__)
-PATH = os.path.join(ROOT_DIR, "models")
+PATH_MODELS = os.path.join(ROOT_DIR, "models")
+PATH_PDF = os.path.join(ROOT_DIR, "pdf")
+PATH_TXT = os.path.join(ROOT_DIR, "txt")
 
 def save_model():
-    
-
     
     model_name = "deepset/roberta-base-squad2"
     
     model = AutoModelForQuestionAnswering.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     
-    joblib.dump(model, f'{PATH}/model_roberta')
-    joblib.dump(tokenizer, f'{PATH}/tokenizer_roberta')
+    joblib.dump(model, f'{PATH_MODELS}/model_roberta')
+    joblib.dump(tokenizer, f'{PATH_MODELS}/tokenizer_roberta')
     
 
+def get_txt(contract_path):
+    pdf = pdfplumber.open(f'{PATH_PDF}/{contract_path}')
+    ls = []
+    for i in pdf.pages:
+        ls.append(i.extract_text())
+    ls = ' '.join(ls)
+    
+    txt_path = contract_path.replace('.pdf', '.txt')
+    
+    with open(f"{PATH_TXT}/{txt_path}", "w") as text_file:
+        text_file.write(ls)
+    
+    return ls
+
+
 def get_output(question):
-    model = joblib.load(f'{PATH}/model_roberta')
-    tokenizer = joblib.load( f'{PATH}/tokenizer_roberta')
+    model = joblib.load(f'{PATH_MODELS}/model_roberta')
+    tokenizer = joblib.load( f'{PATH_MODELS}/tokenizer_roberta')
 
     nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
     QA_input = {
@@ -34,4 +51,5 @@ def get_output(question):
 
 if __name__ == "__main__":
     #save_model()
-    get_output('Why is model conversion important?')
+    #get_output('Why is model conversion important?')
+    get_txt('CreditcardscomInc_20070810_S-1_EX-10.33_362297_EX-10.33_Affiliate Agreement.pdf')
